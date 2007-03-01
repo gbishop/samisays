963944e2280cf2bce1b1e3cfeddca70574c3e6ef
cpreynolds <cpreynolds@569a0b4a-de27-0410-a6a1-594affea032f>
2007-03-01 01:44:19 +0000

    prompts generation

--- trunk/gui/src/GeneratePrompts.py
+++ trunk/gui/src/GeneratePrompts.py
@@ -1,6 +1,20 @@
 import pyTTS
 
+PROMPT_FILE = 'prompts.txt'
+
 tts = pyTTS.Create()
 
-#write to a wave file
-tts.SpeakToWave('spain.wav', 'The rain in Spain falls mainly on the plain.')
+f = open(PROMPT_FILE,'r')
+g = 0
+names = []
+text = []
+for i in f.readlines():
+    if g == 0:
+        text.append(i)
+        g = 1
+    else:
+        names.append(i)
+        g = 0
+
+for i in range(len(names)):
+    tts.SpeakToWave(names[i][:-1],text[i][:-1])
